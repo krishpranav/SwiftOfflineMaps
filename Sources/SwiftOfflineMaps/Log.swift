126d310c1ac007ee6ef32c90f9b49acc06c3b731
krishpranav <krisna.pranav@gmail.com>
2023-09-16 15:39:57 +0530

    Log

---
+++ Sources/SwiftOfflineMaps/Log.swift
@@ -0,0 +1,31 @@
+//
+//  Log.swift
+//  
+//
+//  Created by krisna pranav on 16/09/23.
+//
+
+import Foundation
+
+public struct Log {
+    fileprivate static let tag = "[SwiftOfflineMaps]"
+    
+    fileprivate enum Level: String {
+        case Debug = "[DEBUG]"
+        case Error = "[ERROR]"
+    }
+    
+    fileprivate static func log(_ level: Level, _ message: @autoclosure() -> String, _ error: Error? = nil) {
+        if let error = error {
+            print("\(tag)\(level.rawValue) \(message()) with error \(error)")
+        } else {
+            print("\(tag)\(level.rawValue) \(message())")
+        }
+    }
+    
+    static func debug(message: @autoclosure() -> String, error: Error? = nil) {
+        #if DEBUG
+        log(.Debug, message(), error)
+        #endif
+    }
+}
