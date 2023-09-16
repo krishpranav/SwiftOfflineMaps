7f13bc24d3d7df32ca71194a5078ff18e6d79af2
krishpranav <krisna.pranav@gmail.com>
2023-09-16 16:14:45 +0530

    ZoomableTile

---
+++ Sources/SwiftOfflineMaps/ZoomableTile.swift
@@ -0,0 +1,15 @@
+//
+//  ZoomableTile.swift
+//
+//
+//  Created by krisna pranav on 16/09/23.
+//
+
+import Foundation
+import MapKit
+
+public struct ZoomableTile {
+    let maximumZPath: MKTileOverlayPath
+    
+    let rect: MKMapRect
+}
